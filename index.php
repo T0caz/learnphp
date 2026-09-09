a0f985147a13a7a71a7ef30d759d15326860a9a8
Karl Kundla <karl.kundla@tptlive.ee>
2026-09-09 09:33:08 +0300

    function asi

--- index.php
+++ index.php
@@ -1,19 +1,33 @@
 <?php
-$test = 10;
-$test = 3.1;
-$test = "Hello World";
-$test = "õpilane";
-$test = true;
-$test = false;
-$test = null;
-$lol;
-var_dump($test);
-var_dump($lol);
-$test = [1, 2, "three", true];
-var_dump($test);
-$test = new stdClass();
-$test ->name = "John";
-var_dump($test);
-$num1 = 10;
-$num2 = "10";
-var_dump($num1 === $num2);
+
+function hello() {
+    var_dump('Hello');
+}
+
+function helloName() {
+    var_dump("Hello, $name!");
+}
+
+helloName('Karl');
+helloName('Kundla');
+
+$numbers = [1, 2, 3, 4, 5];
+array_map(function ($number) {
+    return $number * $number;
+}, $numbers);
+$squares = array_map(fn($number) => $number * $number, $numbers);
+var_dump($squares);
+
+function cube($a) {
+    if($a < 0) {
+        return 'Negative number';
+    }
+    return $a * $a * $a;
+    var_dump('This will never be executed');
+}
+
+var_dump(cube(4));
+
+$answer = cube(5);
+$text = "Cube of 5 is $answer!";
+echo $text;

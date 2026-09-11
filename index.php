a73b537c9015350104b58c9514cfc8da4eedc1ea
Karl Kundla <karl.kundla@tptlive.ee>
2026-09-11 14:57:51 +0300

    server html

--- index.php
+++ index.php
@@ -1,34 +1,32 @@
 <?php
 
-class Job {
-    public function task() {
-        for ($i = 0; $i < 10; $i++) {
-            echo "Task is done!" . $i . "\n";
-        }
-    }
-}
-
-class ConsoleLogger {
-    public function log($message) {
-        echo $message . "\n";
-    }
-}
+$name = "Karl";
+$coinFlip = rand(0, 1);
 
 
-class NothingLogger {
-    public function log($message) {
-        // Do nothing
-    }
-}
+?>
 
-class FileLogger {
-    public function log($message) {
-        $file = fopen('log.txt', 'a');
-        fwrite($file, $message . "\n");
-        fclose($file);
-    }
-}
 
-$job = new Job();
-$logger = new FileLogger();
-$job->task();
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello <?php echo $name; ?></h1>
+    <?php if ($coinFlip): ?>
+        <h1>Kull</h1>
+    <?php  else: ?>
+        <h1>Kiri</h1>
+    <?php endif; ?>
+
+    <ul>
+
+
+
+
+</body>
+</html>
